e6329fa2b5ea23842127f93409047b982ced48ef
ItsThankK <tkingsway3@gmail.com>
2022-12-09 09:14:33 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the numbers 1 to 9
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
