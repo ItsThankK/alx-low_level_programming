88cb9afec38e3ffd21e60997a90b2868456bc6c3
ItsThankK <tkingsway3@gmail.com>
2022-12-09 06:29:47 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char alphabet = 'a';
+
+		while (alphabet <= 'z')
+	{
+			printf("%c ", alphabet);
+			alphabet++
+		return (0);
+	}
+}
