e35fa54ff2a3c64e86688636db14739e848f2899
ItsThankK <tkingsway3@gmail.com>
2022-12-09 08:18:17 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabets a to z in lowercase followed by A to Z in uppercase 
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
