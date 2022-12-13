5274c81ad7345d336d39c08768354d881d6963b6
ItsThankK <tkingsway3@gmail.com>
2022-12-12 22:32:46 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{	putchar(_putchar);
+	return (0);
+}
