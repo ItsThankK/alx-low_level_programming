c407c853ddbf8eb929cf9035ca7f096d3064e83f
ItsThankK <tkingsway3@gmail.com>
2022-12-13 04:55:56 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 0; i < 27; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
